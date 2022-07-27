d5139d00f9f657bec6f29f9cb4d5a9246f063d1e
Phoenix Mee <phoenixmee91@gmail.com>
2022-07-27 09:12:34 +0100

    Remove unsafe font import

--- components/layout/index.js
+++ components/layout/index.js
@@ -13,7 +13,6 @@ const Layout = ({ children }) => {
             <Head>
                 <title>Houselawdev</title>
                 <meta name="description" content="Houselawdev" />
-                <link href="http://fonts.cdnfonts.com/css/avenir" rel="stylesheet" preload/>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" preload/>
             </Head>
 
